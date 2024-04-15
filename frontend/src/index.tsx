5f33539348685e15fc04345572146d38e488c996
Greg <grtop@yandex.ru>
2024-04-15 23:44:55 +0300

    правки

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,15 +1,15 @@
 import * as ReactDOM from 'react-dom/client';
 import { StrictMode } from 'react';
-import App from './src/app';
+import App from './app';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Provider } from 'react-redux';
-import { store } from './src/redux/store';
-import { checkUserAction } from './src/redux/userSlice/apiUserActions';
-import { getToken } from './src/services/tokens';
-import HistoryRouter from './src/components/history-router/history-router';
-import browserHistory from './src/browser-history';
-import React from 'react';
+import { store } from './redux/store';
+import { checkUserAction } from './redux/userSlice/apiUserActions';
+import { getToken } from './services/tokens';
+import HistoryRouter from './components/history-router/history-router';
+import browserHistory from './browser-history';
+//import React from 'react';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
